fc57fb71f6426daf647d89858ed1943270b8baab
Rakesh.Kumar <rakesh@highpeaksw.com>
2021-02-03 10:30:56 +0530

    step8, Allow multiple delimiters like this: “//[delim1][delim2]\n” for example “//[*][%]\n1*2%3”

--- src/test/java/com/tdd/app/StringCalculatorTest.java
+++ src/test/java/com/tdd/app/StringCalculatorTest.java
@@ -92,4 +92,10 @@ public class StringCalculatorTest {
         assertEquals(6, stringCalculator.add("//[***]\n1***2***3"));
     }
 
+    @Test
+    public void forMultipleDelimiters()
+    {
+        assertEquals(6, stringCalculator.add("//[*][%]\n1*2%3"));
+    }
+
 }
